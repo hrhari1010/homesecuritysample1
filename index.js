010c91a09b4ccb70b5badf8aae5b29eed72842b6
hrhari1010 <101563443+hrhari1010@users.noreply.github.com>
2022-06-07 12:10:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,58 @@
-const express = require('express')
+const express = require('express');
+const https = require('https');
+//const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const app = express()
 const port = 3000
+//var request = require('request');
+//const req=request()
 
-app.get('/', (req, res) => {
-  res.send('listening and success')
-})
-app.get('/senddata', (req, res) => {
-  res.send('sending data')
-})
-app.get('/senddata2', (req, res) => {
+const options = {
+    hostname: 'harir.pythonanywhere.com',
+    port: 443,
+    path: '/',
+    method: 'GET'
+  }
 var requestOptions = {
-  method: 'GET',
-  redirect: 'follow'
-};
+    method: 'GET',
+    redirect: 'follow'
+  };
+  
+app.get('/senddata', (req, res) => {
+     reqq = https.request(options, ress => {
+        console.log(`statusCode: ${ress.statusCode}`)
+      
+        ress.on('data', d => {
+            ress.return('data')
+        })
+      })
+      
+      reqq.on('error', error => {
+        console.error(error)
+      })
+      
+      reqq.end()
+    
+  })
 
-fetch("https://harir.pythonanywhere.com/", requestOptions)
-  .then(response => response.text())
-  .then(result => res.send(result))
-  .catch(error => console.log('error', error));
- })
+  app.get('/data', function (req, res) {
+    var url = 'https://harir.pythonanywhere.com/api/?temp=30&smoke=44';
+    
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        res.send((data['temperature']));
+    })
+    .catch(err => {
+        res.send(err);
+    });
+});
+app.get('/name', function (req, res) {
+    const name={
+        "data": "hello world"
+    }
+    res.send(name['data'])
+});
 app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening on port ${port}`)
 })
