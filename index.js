917086df66980ca0c5b928379836a609e3e645a0
hrhari1010 <101563443+hrhari1010@users.noreply.github.com>
2022-06-07 11:28:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,17 @@ app.get('/', (req, res) => {
 app.get('/senddata', (req, res) => {
   res.send('sending data')
 })
+app.get('/senddata2', (req, res) => {
+var requestOptions = {
+  method: 'GET',
+  redirect: 'follow'
+};
 
+fetch("https://harir.pythonanywhere.com/", requestOptions)
+  .then(response => response.text())
+  .then(result => res.send(result))
+  .catch(error => console.log('error', error));
+ })
 app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening on port ${port}`)
 })
