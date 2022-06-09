49cc5ffe92d8178ff18f7cba75fc54af4458750c
hrhari1010 <101563443+hrhari1010@users.noreply.github.com>
2022-06-09 13:20:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -78,8 +78,85 @@ app.get('/database',function(req,res){
       // The write was successful...
       res.send("success")
     }
+    
 })
 });
+
+// Fire API
+app.get('/api/fire',function(req,res){
+  let database = firebase.database()
+  t=req.query['t']
+  s=req.query['s']
+  objFI={'Temp':t,'Smoke':s}
+  
+  database.ref("FireInput").set(objFI, function(error) {
+    if (error) {
+      // The write failed...
+      res.send("Failed with error: " + error)
+    } 
+    else {
+      // The write was successful...
+     
+    }
+})//firebase close
+console.log("success in adding Fire Inputs",t,s)
+var urlfire = 'https://harir.pythonanywhere.com/api/fire?t='+t+'&s='+s;
+//https://harir.pythonanywhere.com/api/fire?t=90&s=30
+fetch(urlfire)
+.then(res => res.json())
+.then(data => {
+    let ftyp = data["ftype"]
+    objFO={'FireType':ftyp}
+    database.ref("FireOutput").set(objFO, function(error) {
+    if (error) {
+      // The write failed...
+      res.send("Failed with error: " + error)
+    } else {
+      // The write was successful...
+      res.send("success in adding fire type")
+    } })})
+.catch(err => {
+   res.send(err);
+  });
+}); // api close
+
+
+// Gas API
+app.get('/api/gas',function(req,res){
+  let database = firebase.database()
+  g=req.query['g']
+  objGI={'Gas':g}
+  
+  database.ref("GasInput").set(objGI, function(error) {
+    if (error) {
+      // The write failed...
+      res.send("Failed with error: " + error)
+    } 
+    else {
+      // The write was successful...
+     
+    }
+})//firebase close
+console.log("success in adding Gas Inputs",g)
+var urlgas = 'https://harir.pythonanywhere.com/api/gas?g='+g;
+//https://harir.pythonanywhere.com/api/gas?g=9000
+fetch(urlgas)
+.then(res => res.json())
+.then(data => {
+    let gwa = data["gwait"]
+    objGO={'GasWaitT':gwa}
+    database.ref("GasOutput").set(objGO, function(error) {
+    if (error) {
+      // The write failed...
+      res.send("Failed with error: " + error)
+    } else {
+      // The write was successful...
+      res.send("success in adding fire type")
+    } })})
+.catch(err => {
+   res.send(err);
+  });
+}); // api close
 app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening on port ${port}`)
 })
